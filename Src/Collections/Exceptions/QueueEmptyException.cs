1358b73194d0c274e0b32a96cea9c722cdebcb29
Bojacnik <vitek2004@seznam.cz>
2025-07-23 22:29:50 +0200

    minor tweaks

--- Src/Collections/Exceptions/QueueEmptyException.cs
+++
@@ -1,5 +0,0 @@
-﻿using System;
-
-namespace Bojalib.Collections.Exceptions;
-
-public class QueueEmptyException() : Exception("Queue is empty!");
--- Src/Collections/Exceptions/StackEmptyException.cs
+++
@@ -1,5 +0,0 @@
-﻿using System;
-
-namespace Bojalib.Collections.Exceptions;
-
-public class StackEmptyException() : Exception("Stack is empty!");
