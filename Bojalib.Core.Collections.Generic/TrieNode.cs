df16b160bb8a89c8b28449752f8c6c73126cd712
Bojacnik <vitek2004@seznam.cz>
2025-07-27 13:17:55 +0200

    added trieNode

---
+++ Bojalib.Core.Collections.Generic/TrieNode.cs
@@ -0,0 +1,70 @@
+﻿namespace Bojalib.Core.Collections.Generic;
+
+using System.Linq;
+
+public record TrieNode<TData>()
+{
+    public TData? Data;
+    public required Dictionary<char, TrieNode<TData>> Children { get; init; }
+}
+
+public record TrieTree<TData>
+{
+    public TrieTree()
+    {
+        Root = new TrieNode<TData>
+        {
+            Children = new Dictionary<char, TrieNode<TData>>()
+        };
+    }
+
+    public void InsertWord(string word, TData data)
+    {
+        TrieNode<TData> current = Root;
+
+        for (var i = 0; i < word.Length; i++)
+        {
+            char letter = word[i];
+
+            if (!current.Children.TryGetValue(letter, out TrieNode<TData>? value))
+            {
+                value = new TrieNode<TData>
+                {
+                    Children = new Dictionary<char, TrieNode<TData>>()
+                };
+                current.Children.Add(letter, value);
+            }
+
+            current = value;
+
+            if (i == word.Length - 1)
+            {
+                current.Data = data;
+            }
+        }
+    }
+
+    public TData? FindWord(string word)
+    {
+        TrieNode<TData> current = Root;
+
+        for (var i = 0; i < word.Length; i++)
+        {
+            char letter = word[i];
+
+            if (!current.Children.TryGetValue(letter, out TrieNode<TData>? child))
+            {
+                return default;
+            }
+            current = child;
+
+            if (i == word.Length - 1)
+            {
+                return current.Data;
+            }
+        }
+        return default;
+    }
+
+    private TrieNode<TData> Root { get; init; }
+}
