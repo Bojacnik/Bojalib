83d2e7384a70d83a5354a1e5879b0ece723aa942
Vít Němec <vitek2004@seznam.cz>
2025-09-29 22:21:44 +0200

    dump

---
+++ Src/Sorting/Dfs.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Bojalib.Collections;
+
+namespace Bojalib.Sorting;
+
+public static class Dfs<T>
+{
+    public static void Traverse(Node<T> g)
+    {
+        if (g != null) traverse(g);
+    }
+
+    private static List<T> traverse(Node<T> n)
+    {
+        List<T> output = new();
+        foreach (Node<T> node in n.Children)
+        {
+            traverse(node);
+        }
+
+        return output;
+    }
+}
